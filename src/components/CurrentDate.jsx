49321406cfff1f0d16bac3f10670f218ee4f3d46
mouhamed <mouhamed.mbaye1@outlook.com>
2023-08-10 15:35:57 +0200

    toLocaleDateString Not needed on currentDate

--- src/components/CurrentDate.jsx
+++ src/components/CurrentDate.jsx
@@ -3,14 +3,11 @@ import PropTypes from 'prop-types'
 import { monthsInLetter } from '../data/monthInLetter';
 
 function CurrentDate({ month, year, locale }) {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const monthIndex = month.toLocaleDateString(locale, options)
-    const formattedYear = year.toLocaleDateString(locale, options)
     const lang = locale.split('-')[0]
 
     return (
         <div className='current-date'>
-            {`${monthsInLetter[lang][monthIndex]} ${formattedYear}`}
+            {`${monthsInLetter[lang][month]} ${year}`}
         </div>
     )
 }
